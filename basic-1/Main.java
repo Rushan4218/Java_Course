1cb03f39bd4fb19999e61ecb19ee9a48692c2ca8
Rushan <rushanbuyorb@gmail.com>
2026-01-27 17:39:53 +0545

    Day 1 and Day 2

---
+++ basic-1/Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    String firstName = args[0];
+    String lastName = args[1];
+
+    System.out.println("FirstName: " + firstName);
+    System.out.println("LastName: " + lastName);
+  }
+}
---
+++ working-with-classes-2/Main.java
@@ -0,0 +1,43 @@
+// learning about classes and constructors 
+
+class Student {
+  String name;
+  int age;
+
+  // Constructor
+  public Student(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public void printInfo() {
+    System.out.println("Student : " + this.name + ", Age: " + this.age);
+  }
+}
+
+class Faculty {
+  String name;
+  int duration;
+
+  public void setData(String name, int duration) {
+    this.name = name;
+    this.duration = duration;
+  }
+
+  public void printInfo() {
+    System.out.println("Faculty: " + this.name + ", Duration: " + this.duration);
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    // Creating objects
+    Student rushan = new Student("Rushan", 21);
+    Faculty csFaculty = new Faculty();
+    csFaculty.setData("Computer Science", 4);
+
+    // Printing info
+    rushan.printInfo();
+    System.out.println("Faculty: " + csFaculty.name + ", Duration: " + csFaculty.duration + " years");
+  }
+}
